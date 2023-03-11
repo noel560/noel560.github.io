619f44205ecb013b8e6760df4ad16cd00e393d5d
Noel <keresztesnnoel0715@gmail.com>
2023-03-11 15:26:17 +0100

    v1

---
+++ index.js
@@ -0,0 +1,12 @@
+function function1() {
+    window.open("https://www.youtube.com/channel/UCDvFN5O8vBUY41sigmuho5Q")
+}
+function function2() {
+    window.open("https://github.com/noel560")
+}
+function function3() {
+    window.open("https://noel54.itch.io/")
+}
+function function4() {
+    window.open("https://www.spigotmc.org/members/noel071554.1232082/")
+}
