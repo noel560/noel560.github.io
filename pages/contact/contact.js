bf344ada06180f5664de54a14a9e56287a2ef789
Noel <82030459+noel560@users.noreply.github.com>
2023-11-21 17:04:03 +0100

    Update

---
+++ pages/contact/contact.js
@@ -0,0 +1,15 @@
+function functionimage() {
+    document.location.href = "/index.html"
+}
+
+function functionhome() {
+    document.location.href = "/index.html"
+}
+
+function functioncontact() {
+    document.location.href = "/pages/contact/contact.html"
+}
+
+function functiongames() {
+    document.location.href = "/pages/games/games.html"
+}
